535009c9d5185801c99dc18db3ec42db1a47fee9
AliceBen <angelQin0704@163.com>
2020-07-22 17:47:08 +0800

    feat: userReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // react Hooks 写法
-import React, { useState, useEffect, createContext, useContext } from 'react'
+import React, { useState, useEffect, createContext, useContext, useReducer } from 'react'
 
 const CountContext = createContext()
 
-function Example () {
+function Example() {
     // useState hooks函数，声明状态变量
     // react Hooks 不能出现在条件判断语句中，因为它必须有完全一样的渲染顺序
     // 解构赋值
-    const [ count, setCount ] = useState(0) // (0)初始值
-    const [ age, setAge ] = useState(18)
-    const [ sex, setSex ] = useState('女')
+    const [count, setCount] = useState(0) // (0)初始值
+    const [age, setAge] = useState(18)
+    const [sex, setSex] = useState('女')
 
     // 每次状态更新时执行   userEffect(副作用)
     // react首次渲染和之后的每次渲染都会调用一遍 userEffect 函数，而之前我们要用两个生命周期函数分别表示首次渲染(componentDidMount)和更新导致的重新渲染(componentDidUpdate)
@@ -23,7 +23,7 @@ function Example () {
         return () => {
             console.log('===========')
         }
-    },[count])  // 每次 count 发生变化都进行解绑,实现componentWillUnmount的效果
+    }, [count])  // 每次 count 发生变化都进行解绑,实现componentWillUnmount的效果
 
 
     // 父子组件传值 useContext // 对它所包含的组件树提供全局共享数据的技术
@@ -36,28 +36,24 @@ function Example () {
     // 2. 创建 CountContext.provider标签，设置value元素赋值，标签内引入需要获取数据的组件，继而上下文可用
     // 3. 引入 useContext
     // 4. 创建需要使用变量的组件，useContext(countContext) 取值
-    
+
     return (
-       <div>
-           <p>You clicked {count} times</p>
-           <button onClick={() => {setCount(count + 1)}}>click me</button>
-           {/* 把count变量允许跨层级实现传递和使用 */}
-           <CountContext.Provider value={count}>
-               <Counter />
-           </CountContext.Provider>
-       </div> 
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => { setCount(count + 1) }}>click me</button>
+            {/* 把count变量允许跨层级实现传递和使用 */}
+            <CountContext.Provider value={count}>
+                <Counter />
+            </CountContext.Provider>
+        </div>
     )
 }
 
 // useContext 接收上下文变量
 function Counter() {
     const count = useContext(CountContext)
-return (<h2>{count}</h2>)
+    return (<h2>{count}</h2>)
 }
-
-
-
-
 
 // 原始写法
 // import React, { Component } from 'react'
@@ -91,4 +87,30 @@ return (<h2>{count}</h2>)
 //     }
 // }
 
-export default Example
+// export default Example
+
+
+
+
+// useReducer
+function ReducerDemo() {
+    const [count, dispatch] = useReducer((state, action) => {
+        switch (action) {
+            case 'add':
+                return state + 1
+            case 'sub':
+                return state - 1
+            default:
+                return state
+        }
+    },0)
+    return (
+        <div>
+            <h2>现在的分数是{count}</h2>
+            <button onClick={() => dispatch('add')}>Increment</button>
+            <button onClick={() => dispatch('sub')}>Decrement</button>
+        </div>
+    )
+}
+
+export default ReducerDemo
